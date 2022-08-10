435d3507a29310e517e8984946856b00b07432c8
Michael Loughman <mcloughman@gmail.com>
2022-08-10 15:57:30 -0700

    We modify the package.json to add proxy property and local server as the value. Doing this changes the wat that we set up the fetch request in our Home.js. These are changes that NetNinja makes in lieu of using cors package so that our front end can successfully call our backend server. In Home.js we fetch data from backend and if successful, we loop over it and pass each workout object into the newly created WorkoutDetails component.

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,29 @@
+import { useState, useEffect } from "react";
+
+// components
+import WorkoutDetails from "../components/WorkoutDetails";
+
 const Home = () => {
+  const [workouts, setWorkouts] = useState(null);
+  useEffect(() => {
+    const fetchWorkouts = async () => {
+      const response = await fetch("/api/workouts");
+      const json = await response.json();
+
+      if (response.ok) {
+        setWorkouts(json.workouts);
+      }
+    };
+    fetchWorkouts();
+  }, []);
   return (
     <div className="home">
-      <h2>Home</h2>
+      <div className="workouts">
+        {workouts &&
+          workouts.map((workout) => (
+            <WorkoutDetails key={workout._id} workout={workout} />
+          ))}
+      </div>
     </div>
   );
 };
