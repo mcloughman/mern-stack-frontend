4c14cdbfd1048124a2e732c7b853c436f9c0268e
Michael Loughman <mcloughman@gmail.com>
2022-08-10 21:24:36 -0700

    Net Ninja MERNStack lesson 11. Use react context to keep track of state globally throught our app. This is beyond my level. The Ninja says that this will make it so we don't have to pass state down through props to various components. I will have to go back to this topic after the project is completed. I do what I'm told in this section.

---
+++ src/context/WorkoutContext.js
@@ -0,0 +1,27 @@
+import { createContext, useReducer } from "react";
+
+export const WorkoutsContext = createContext(); // creates new context
+
+export const workoutsReducer = (state, action) => {
+  switch (action.type) {
+    case "SET_WORKOUTS":
+      return {
+        workouts: action.payload,
+      };
+    case "CREATE_WORKOUT":
+      return {
+        workouts: [action.payload, ...state.workouts],
+      };
+  }
+};
+
+export const WorkoutsContextProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(workoutsReducer, { workouts: null });
+
+  return (
+    // needs to wrap parts of app that need access to context
+    <WorkoutsContext.Provider value={{ ...state, dispatch }}>
+      {children}
+    </WorkoutsContext.Provider>
+  );
+};
