e69a81a96f0941d7b4db694729fbe807d47cbf28
Michael Loughman <mcloughman@gmail.com>
2022-08-13 07:03:25 -0700

    Create useSignup hook -- save user to local storage, update the authContext for type 'LOGIN'. Import and implement useSignup in Signup.js. Test in browser.

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -1,12 +1,14 @@
 import { useState } from "react";
+import { useSignup } from "../hooks/useSignup";
 
 const Signup = () => {
   const [email, setEmail] = useState();
   const [password, setPassword] = useState();
+  const { signup, isLoading, error } = useSignup();
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    console.log(email, password);
+    await signup(email, password);
   };
 
   return (
@@ -25,7 +27,8 @@ const Signup = () => {
         onChange={(e) => setPassword(e.target.value)}
         value={password}
       />
-      <button>Signup</button>
+      <button disabled={isLoading}>Signup</button>
+      {error && <div className="error">{error}</div>}
     </form>
   );
 };
